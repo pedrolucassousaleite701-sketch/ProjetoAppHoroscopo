b2d72da24087c85d4f9bde0f7b5fbf718007ae4c
Pedro Leite <pedro67701356@alunos.to.senac.br>
2026-09-22 17:54:00 -0300

    criação das telas com imagem  de fundo

---
+++ Horoscopo/src/main/java/com/mycompany/horoscopo/Horoscopo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.horoscopo;
+
+/**
+ *
+ * @author PedroLeite
+ */
+public class Horoscopo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
